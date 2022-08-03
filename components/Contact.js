f92dd0591fbeccad3603525fa37654c9973dfadc
mdtamizuddin <tamizrabbi@gmail.com>
2022-08-03 22:40:51 +0600

    contact form done

--- components/Contact.js
+++ components/Contact.js
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-
+import contact from '../Accets/Image/contact.png'
 
 
 const Contact = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit } = useForm();
     const onSubmit = data => {
         toast.custom((t) => (
             <div
@@ -27,38 +28,43 @@ const Contact = () => {
 
 
     return (
-        <div className="py-10 container mx-auto">
-            <div className="bg-secondary  lg:w-[40%] rounded-xl px-5 py-10 h-auto">
-                <h4 className="flex  items-center  text-accent uppercase text-sm">
-                    <div className="sm-row mr-4"></div>
-                    let's get started
-                </h4>
-                <h1 className="lg:text-5xl header-head md:text-4xl text-5xl  mt-3 font-extrabold">
-                    Get a Free <br />
-                    Consultation
-                </h1>
-                <p className="text-accent text-mini mt-2">
-                    We always try to implement our creative ideas at the highest level. <br />
-                    Tell us about your project and we will make it work.
-                </p>
+        <div className="lg:py-20 container mx-auto">
+            <section className="flex justify-between">
+                <div className="bg-secondary  lg:w-[40%] rounded-xl px-5 py-10 h-auto">
+                    <h4 className="flex  items-center  text-accent uppercase text-sm">
+                        <div className="sm-row mr-4"></div>
+                        let's get started
+                    </h4>
+                    <h1 className="lg:text-5xl header-head md:text-4xl text-5xl  mt-5 font-extrabold">
+                        Get a Free <br />
+                        Consultation
+                    </h1>
+                    <p className="text-accent text-mini mt-2">
+                        We always try to implement our creative ideas at the highest level. <br />
+                        Tell us about your project and we will make it work.
+                    </p>
 
-                <form
-                    onSubmit={handleSubmit(onSubmit)}
-                    className="mt-14 p-4 contact-form">
-                    <div className="grid grid-cols-2 gap-5">
-                        <input
-                            {...register("name", { required: true })}
-                            type="text" placeholder="Name" />
-                        <input
-                            {...register("email", { required: true })}
-                            className="bg-transparent" type="email" placeholder="Email Adress" />
-                    </div>
-                    <textarea
-                        {...register("message", { required: true })}
-                        placeholder="Message"></textarea>
-                    <button type="submit" className="btn mt-10 btn-outline btn-primary btn-md rounded-full px-7">Send Message</button>
-                </form>
-            </div>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="mt-14 p-4 contact-form">
+                        <div className="grid grid-cols-2 gap-5">
+                            <input
+                                {...register("name", { required: true })}
+                                type="text" placeholder="Name" />
+                            <input
+                                {...register("email", { required: true })}
+                                className="bg-transparent" type="email" placeholder="Email Adress" />
+                        </div>
+                        <textarea
+                            {...register("message", { required: true })}
+                            placeholder="Message"></textarea>
+                        <button type="submit" className="btn mt-10 btn-outline btn-primary btn-md rounded-full px-7">Send Message</button>
+                    </form>
+                </div>
+                <div className="w-[50%]">
+                    <Image src={contact} />
+                </div>
+            </section>
         </div>
     );
 }
