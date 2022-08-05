3560fdc8be68e7f814dbefa0e33eecb5b1667e1c
mdtamizuddin <tamizrabbi@gmail.com>
2022-08-06 02:08:30 +0600

    changes on portfolio card

--- components/Cards/PortCard.js
+++ components/Cards/PortCard.js
@@ -12,7 +12,10 @@ const PortCard = () => {
             <div className="overlay absolute top-0 bg-secondary w-full h-full border-gray-700 
             animate__fadeInDown animate__animated border
             ">
-                <p className="text-xl text-accent">
+                <Image src={'https://firebasestorage.googleapis.com/v0/b/agency-89d42.appspot.com/o/web%2Fwebsite-4.png?alt=media&token=44023fdf-001c-4f86-a0e0-6df8361dbd59'} 
+                width={400} height={300}
+                alt=""/>
+                <p className="text-xl text-accent mt-5">
                     This Web Site Designed By Dangerous Programmers
                 </p>
                 <button className="mt-7 py-1 border-b-2 hover:border-secondary text-primary border-primary transition delay-300 ">Read More
